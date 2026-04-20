0e2ac82be4930cb683d32d71a35841703ebc3dd2
Tempmarcos <marcostempdesouto@gmail.com>
2026-04-20 19:10:14 -0300

    alteração permissões pra ficar mais alinhado ao DDD

--- src/modules/user/domain/value-objects/Permissoes.ts
+++ src/modules/user/domain/value-objects/Permissoes.ts
@@ -20,34 +20,53 @@ export enum permissions  {
     CriarTarefas = 'criarTarefas',
 
     //TURMAS
+    VerTurmas = 'verTurmas',
+    CriarTurmas = 'criarTurmas',
+    DeletarTurmas = 'deletarTurmas',
+    EditarTurmas ='editarTurmas',
+    FazerChamada = 'fazerChamada',
 
     //FUNCIONARIOS
+    VerFuncionarios = 'verFuncionarios',
+    CriarFuncionarios = 'criarFuncionarios',
+    DeletarFuncionarios = 'deletarFuncionarios',
+    EditarFuncionarios ='editarFuncionarios', 
+    VerInfoFuncionario = 'verInfoFuncionario',
 
     //FINANCEIRO
+    VerFinanceiro = 'verFinanceiro',
+    CriarRegistro = 'criarRegistro',
+    DeletarRegistro = 'deletarRegistro',
+    EditarRegistro = 'editarRegistro',
 }
 
 export class Permissoes extends ValueObject<string[]> {
     private constructor(valor: string[]){
         super(valor)
     }
-    static criar(permissoes: string[]){
+    static criar(permissoes: permissions[]){
         const resultado = Validador.combinar(
-        Validador.tamanhoMaximoArray(permissoes, Object.values(permissions).length, "Permissões"),
-    );
-    if (!resultado.valido) {
-        throw new Error(resultado.erro);
-    }
-    for(const permissao of permissoes){
-        if(!Object.values(permissions).includes(permissao) && permissao != '') throw new Error('Permissão inválida')
-    }
+            Validador.tamanhoMaximoArray(permissoes, Object.values(permissions).length, "Permissões"),
+        );
+        if (!resultado.valido) {
+            throw new Error(resultado.erro);
+        }
+        for(const permissao of permissoes){
+            if(!Object.values(permissions).includes(permissao)) throw new Error('Permissão inválida')
+        }
 
-    return new Permissoes(permissoes)
+        return new Permissoes([...new Set(permissoes)])
     }
-    static giveAllPermissoes(userPermissions : string[]){
-        userPermissions.length = 0; 
-        userPermissions.push(...Object.values(permissions));
+    static todasAsPermissoes(): Permissoes {
+        return new Permissoes([...Object.values(permissions)]);
     }
-    static comparePermissions(permissions : string[], userPermissions : string[]): boolean{
-       return permissions.every(permissao => userPermissions.includes(permissao))
+    public possuiTodasDe(outra: Permissoes): boolean{
+       return this.valor.every(permissao => outra.valor.includes(permissao))
+    }
+    public possui(permissao: permissions): boolean {
+    return this.valor.includes(permissao);
+    }
+    public possuiAlgumaDe(outra: Permissoes): boolean {
+        return outra.valor.some(p => this.valor.includes(p));
     }
 }
