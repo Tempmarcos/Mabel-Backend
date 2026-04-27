bf1129d09d60207cb1a8ac63cd9fe44b2af31d76
Tempmarcos <marcostempdesouto@gmail.com>
2026-04-27 18:58:59 -0300

    criarUserUseCase correção

---
+++ src/modules/user/application/use-cases/CriarUserUseCase.ts
@@ -0,0 +1,9 @@
+import { UsuarioRepository } from "../../domain/UserRepository";
+
+export class CriarUserUseCase {
+    constructor(userRepository: UsuarioRepository){}
+
+    async execute(){
+
+    }
+}
